24983b2389207b027aa4a8f8c3e68775cc7e1aea
baoloi2002 <loitan1042002@gmail.com>
2021-04-06 21:10:46 +0700

    add school.h
    add data struct

---
+++ A Course Registration System/school.h
@@ -0,0 +1,30 @@
+#ifndef _School_
+#define _School_
+
+using namespace std;
+
+struct DateOfBirth{
+    int day, month, year;
+};
+
+struct Student{
+    char* firstName,* lastName,* No,* studentID,* Gender,* socialID;
+    DateOfBirth birth;
+
+};
+
+struct Class{
+    char* className;
+    Student * studentList;  // Dynamic array
+};
+
+struct SchoolYear{
+    int year;
+    Class* classList;   //Dynamic array
+};
+
+struct school{
+    SchoolYear* yearList;   //Dynamic array
+};
+
+#endif // _School_
